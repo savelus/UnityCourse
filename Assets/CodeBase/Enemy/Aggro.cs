f197fcaa1103e95269b7f92444794b5d99bdac0a
Savelus <savel@d-element.ru>
2023-08-30 17:55:47 +0500

    05/11а

---
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+	public class Aggro : MonoBehaviour
+	{
+		public TriggerObserver TriggerObserver;
+		public AgentMoveToPlayer Follow;
+		
+		private void Start()
+		{
+			TriggerObserver.TriggerEnter += TriggerEnter;
+			TriggerObserver.TriggerExit += TriggerExit;
+
+			SwitchFollowOff();
+		}
+		private void TriggerExit(Collider obj) =>
+			SwitchFollowOff();
+		private void TriggerEnter(Collider obj) =>
+			SwitchFollowOn();
+		private void SwitchFollowOff() =>
+			Follow.enabled = false;
+		private void SwitchFollowOn() =>
+			Follow.enabled = true;
+	}
+}
